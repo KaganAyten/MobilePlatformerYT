d86205791e15e014c0869b3daf629a5a29473bd8
Kagan Ayten <kaan.aytn@hotmail.com>
2021-07-30 16:51:35 +0300

    Giriş-KarakterKontrolü

---
+++ Platformer/Assets/Kodlar/KarakterKontrol.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KarakterKontrol : MonoBehaviour
+{
+    [SerializeField]
+    private float hareketHizi;
+
+    private Animator anim;
+    private SpriteRenderer sRenderer;
+    int yon;
+    void Start()
+    {
+        anim = gameObject.GetComponent<Animator>();
+        sRenderer = gameObject.GetComponent<SpriteRenderer>();
+    }
+    void Update()
+    {
+        transform.Translate(yon * hareketHizi * Time.deltaTime, 0, 0);
+        if (yon != 0)
+        {
+            anim.SetBool("kosuyor", true);
+            if (yon > 0)
+            {
+                sRenderer.flipX = false;
+            }
+            else if (yon < 0)
+            {
+                sRenderer.flipX = true;
+
+            }
+        }
+        else
+        {
+
+            anim.SetBool("kosuyor", false);
+        }
+    }
+    public void YonKarar(int fonksiyonYon)
+    {
+        this.yon = fonksiyonYon;
+    }
+
+}
